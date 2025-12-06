ba4eebfd38ea63e7a421f51e0e11cf1700ab8599
BernardRecettes <bgaudreault63@gmail.com>
2025-12-06 14:29:18 -0500

    Update app.py
    Correction du nom du fichier "souvenirs"

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ with col2:
     st.page_link("pages/3_Recherche_par_ingredients.py", label="🥕 Recherche par ingrédients")
     st.page_link("pages/0_HOMMAGE.py", label="💐 Page Hommage")
     st.page_link("pages/5_preface.py", label="📖 Préface")
-    st.page_link("pages/souvenirs.py", label="📚 Souvenirs de famille")
+    st.page_link("pages/6_souvenirs.py", label="📚 Souvenirs de famille")
 
     st.markdown("---")
     st.markdown("Pour commentaires ou suggestions :")
@@ -64,6 +64,7 @@ if st.button("💬 Partager un souvenir"):
         "?subject=Souvenir%20pour%20le%20Livre%20d%27Adrienne)",
         unsafe_allow_html=True,
     )
+
 
 
 
