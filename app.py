d3e011bbfd1a20a55f47bc95788e64cf9d7a2a47
BernardRecettes <bgaudreault63@gmail.com>
2025-12-06 08:46:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,10 @@ with col1:
     st.page_link("pages/2_Recherche_par_categorie.py", label="📂 Recherche par catégorie")
     st.page_link("pages/3_Recherche_par_ingredients.py", label="🥕 Recherche par ingrédients")
 
-#with col2:
-    #st.page_link("pages/0_HOMMAGE.py", label="💐 Page Hommage")
+with col2:
+    st.page_link("pages/0_HOMMAGE.py", label="💐 Page Hommage")
     #st.page_link("pages/5_Preface.py", label="📖 Préface")
+
 
 
 
